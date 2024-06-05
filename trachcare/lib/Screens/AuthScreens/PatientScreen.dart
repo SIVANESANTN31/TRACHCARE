56dcd0492b1eae7ef18749037eefca2313c431d1
Sudhar08 <99385366+sudhar08@users.noreply.github.com>
2024-06-05 14:40:03 +0530

    feat:Works fine

---
+++ trachcare/lib/Screens/AuthScreens/PatientScreen.dart
@@ -0,0 +1,15 @@
+import "package:flutter/material.dart";
+
+class patientScreenlogin extends StatefulWidget {
+  const patientScreenlogin({super.key});
+
+  @override
+  State<patientScreenlogin> createState() => _patientScreenloginState();
+}
+
+class _patientScreenloginState extends State<patientScreenlogin> {
+  @override
+  Widget build(BuildContext context) {
+    return Text("Hello");
+  }
+}
