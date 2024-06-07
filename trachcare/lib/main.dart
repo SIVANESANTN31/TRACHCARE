52323eb5fa974349682e3014f67ae0460ea848f5
SIVANESAN S <112241784+SIVANESAN2061@users.noreply.github.com>
2024-06-07 15:46:35 +0530

    s

--- trachcare/lib/main.dart
+++ trachcare/lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
  
 
 
-    return MaterialApp(
+    return MaterialApp( 
       debugShowCheckedModeBanner: false,
       theme: ThemeData(textTheme: GoogleFonts.ibmPlexSansTextTheme()),
       
