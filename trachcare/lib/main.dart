ad29d11dd52b91e2fb6d1a659f45448967993dd2
SIVANESAN S <112241784+SIVANESAN2061@users.noreply.github.com>
2024-06-13 14:06:23 +0530

    start

--- trachcare/lib/main.dart
+++ trachcare/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'SplashScreen.dart';
 
 import 'package:trachcare/Screens/AuthScreens/WelcomePage.dart';
 
