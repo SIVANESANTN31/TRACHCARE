ed5dc8209ca492da75cbcbb007acddda25e3ae74
SIVANESAN S <112241784+SIVANESANTN31@users.noreply.github.com>
2024-08-12 20:25:30 +0530

    commit

--- trachcare/lib/Api/Apiurl.dart
+++ trachcare/lib/Api/Apiurl.dart
@@ -2,10 +2,4 @@ const ip = "192.168.1.7";
 
 String PatientLoginurl = "https://$ip/Trachcare/auth/patientAuth/PatientLogin.php";
 String getpatientdetialsurl = "https://$ip/Trachcare/Patient/getdetials.php";
-
-
-
-
-
-
 String DoctorLoginUrl = "https://$ip/Trachcare/auth/DoctorAuth/DoctorLogin.php";
